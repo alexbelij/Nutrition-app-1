1a48f676ef47a2347b1d0888702adca3bb33a4fa
kharioki <antolisah@gmail.com>
2020-06-13 15:50:25 +0300

    listing food items

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nutrition App',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: HomePage(),
     );
   }
@@ -29,41 +26,164 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF21BFBD),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 45, left: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  color: Colors.white,
-                  onPressed: () {},
+      body: Stack(children: <Widget>[
+        Scaffold(
+          backgroundColor: Color(0xFF21BFBD),
+          body: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 45, left: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                    Container(
+                      width: 125.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Feather.sliders),
+                            color: Colors.white,
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(AntDesign.appstore_o),
+                            color: Colors.white,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  width: 125.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Healthy',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Food',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: MediaQuery.of(context).size.height - 200,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(75),
+              ),
+            ),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(top: 20),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25, right: 20, top: 40),
+                child: Container(
+                  child: Column(
                     children: <Widget>[
-                      IconButton(
-                        icon: Icon(Feather.sliders),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Icon(AntDesign.appstore_o),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
+                      _buildFoodItem(
+                          'assets/images/plate1.png', 'Salmon bowl', '\$24.00'),
+                      _buildFoodItem(
+                          'assets/images/plate2.png', 'Spring bowl', '\$22.00'),
+                      _buildFoodItem(
+                          'assets/images/plate3.png', 'Veggie bowl', '\$32.00'),
+                      _buildFoodItem('assets/images/plate4.png', 'Chicken bowl',
+                          '\$34.00'),
+                      _buildFoodItem(
+                          'assets/images/plate6.png', 'Steak bowl', '\$26.00'),
+                      _buildFoodItem(
+                          'assets/images/plate5.png', 'Corn bowl', '\$24.00')
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
           ),
-        ],
+        )
+      ]),
+    );
+  }
+
+  Widget _buildFoodItem(String imgPath, String foodName, String price) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: InkWell(
+        onTap: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Hero(
+                    tag: imgPath,
+                    child: Image(
+                      image: AssetImage(imgPath),
+                      fit: BoxFit.cover,
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        foodName,
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 17,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        price,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+              icon: Icon(Ionicons.ios_add),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
